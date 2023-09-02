2ffb346d43f3076102b2ffc1a79279c0863c9a99
arifinabdillah <143859978+arifinabdillah@users.noreply.github.com>
2023-09-02 10:59:33 +0700

    Add files via upload
    upload file

---
+++ pelcod.py
@@ -0,0 +1,37 @@
+nama_saya = str(input("masukkan nama anda = "))
+print(nama_saya, "bertipe", type(nama_saya))
+
+umur = int(input("masukkan umur anda = "))
+print(umur, "bertipe", type(umur))
+
+IPK = float(input("masukkan IPK anda = "))
+print(IPK, "bertipe", type(IPK))
+
+menikah = bool(input("apakah anda sudah menikah = "))
+print("menikah?",menikah, "bertipe", type(menikah))
+
+# matkul_diambil = ["alpro", "sig"]
+# print("Matkul : ",matkul_diambil, "bertipe", type(matkul_diambil))
+
+# matkul_diambil2 = ["web", "pemvis"]
+# print("Matkul lain : ",matkul_diambil2, "bertipe", type(matkul_diambil2))
+
+# matkul_diambil3 = ["Alpro", "Alpro", "Pemweb", "Data Sciense", "Data Sciense"]
+# print("Matkul Lain-Lain : ",matkul_diambil3, "bertipe", type(matkul_diambil3))
+
+# print()
+# print()
+
+# print(17+7)
+# print(17*7)
+# print(17-7)
+# print(17/7)
+# print(17//7)
+# print(17%7)
+
+# nama_universitas = "Universitas Trunojoyo"
+# print(nama_universitas)
+
+# ama_universitas = str(input("masukkan nama universitas = "))
+# print(nama_universitas)
+
